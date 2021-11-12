0dc030e6e72d334005e0f342a9842b4ab09945b6
Derrick Eberlein <91962608+ReverendTrivium@users.noreply.github.com>
2021-11-12 09:46:02 -0500

    Connected New and Load Lists Buttons from MainActivity to open up LoadList and NewList Activities

--- app/src/main/java/com/example/fromstoretocore/MainActivity.java
+++ app/src/main/java/com/example/fromstoretocore/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -29,15 +31,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void LoadList(View view) {
-        Intent intent = new Intent(this, LoadList.class);
-        //NewList temp = new NewList();
-        // Check to see if their are any saved Lists //
-        // Placeholder for now, No function for checked added yet
-        Toast toast = Toast.makeText(getApplicationContext(), "No Saved Lists Found", Toast.LENGTH_SHORT);
-        toast.show();
+    Intent intent = new Intent(this, LoadList.class);
+    //NewList temp = new NewList();
+    // Check to see if their are any saved Lists //
+    // Placeholder for now, No function for checked added yet
+    Toast toast = Toast.makeText(getApplicationContext(), "No Saved Lists Found", Toast.LENGTH_SHORT);
+    toast.show();
 
-        startActivity(intent);
+    startActivity(intent);
     }
+
     public void OutputDisplay(View view) {
 
     }
