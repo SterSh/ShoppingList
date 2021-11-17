065d32a76fa23d20ad8212603263d107247a6432
SterSh <sterlingshaw@gmail.com>
2021-11-16 17:34:20 -0700

    add functionality to display items on the ListView. updated the interface a little

--- app/src/main/java/com/example/fromstoretocore/NewList.java
+++ app/src/main/java/com/example/fromstoretocore/NewList.java
@@ -2,15 +2,23 @@ package com.example.fromstoretocore;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NewList extends AppCompatActivity {
-    private ArrayList<EditText> items = new ArrayList<>();
+
+    private List<String> items = new ArrayList<>();
+    private EditText itemInput;
+    private ListView lv_groceryList;
+
+    ArrayAdapter listAdapter;
 
     public NewList() {
 
@@ -20,12 +28,25 @@ public class NewList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_list);
+
+        itemInput = findViewById(R.id.itemInput);
+
+
+        lv_groceryList = findViewById(R.id.lv_groceryList);
+
+        listAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1 , items);
+        lv_groceryList.setAdapter(listAdapter);
+
+
     }
 
     public void AddItem(View view) {
-        EditText newItem = findViewById(R.id.itemInput);
+        String newItem = itemInput.getText().toString();
         items.add(newItem);
-        Toast toast = Toast.makeText(getApplicationContext(), "Item Added", Toast.LENGTH_SHORT);
+
+        listAdapter.notifyDataSetChanged();
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Item Added" + newItem, Toast.LENGTH_SHORT);
         toast.show();
     }
 }
